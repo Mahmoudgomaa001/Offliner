a44d5ef1ae0fad6ba55f2dcdf2c9388f1a2385ff
Noureddine Laassari <nlaassari@gmail.com>
2024-04-04 23:16:26 +0000

    download 1080p and merge with ffmpeg

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import fs from "fs";
+import cp from "child_process";
+import ffmpeg from "ffmpeg-static";
 import path from "path";
 import ytdl from "ytdl-core";
 
@@ -24,31 +26,18 @@ app.get("/video/info", async (req, res) => {
 app.get("/video/download", async (req, res) => {
   const { url } = req.query;
 
-  const info = await ytdl.getInfo(url);
-  let format = ytdl.chooseFormat(info.formats, { quality: "highest" });
+  const stream = await downloadHighestQualityVideo(url, res);
 
-  res.attachment(videoName(info.videoDetails.title, format.container));
-  res.header("Content-Length", format.contentLength);
-  res.header("Content-Type", format.mimeType.split(";")[0]);
-
-  ytdl.downloadFromInfo(info, { format }).pipe(res);
+  stream.pipe(res);
 });
 
 // Download file to disk then stream result to response
 app.get("/video/download-first", async (req, res) => {
   const { url } = req.query;
 
-  const info = await ytdl.getInfo(url);
-  const format = ytdl.chooseFormat(info.formats, {
-    quality: "highestvideo",
-    filter: (f) => f.hasAudio && f.hasVideo,
-  });
   const writeStream = fs.createWriteStream(TMP_FILE);
 
-  res.attachment(info.videoDetails.title.concat(".", format.container));
-  res.header("Content-Type", format.mimeType.split(";")[0]);
-
-  const downloadStream = ytdl.downloadFromInfo(info, { format });
+  const downloadStream = await downloadHighestQualityVideo(url, res);
 
   downloadStream.pipe(writeStream);
   writeStream.on("error", (err) => {
@@ -56,7 +45,7 @@ app.get("/video/download-first", async (req, res) => {
   });
 
   downloadStream.on("end", () => {
-    const size = fs.statSync("file").size
+    const size = fs.statSync("file").size;
     res.header("Content-Length", size);
     const readStream = fs.createReadStream(TMP_FILE);
 
@@ -78,6 +67,73 @@ app.listen(process.env.PORT || 3000, () => {
   console.log("Server running!");
 });
 
-function videoName(title, ext) {
-  return title.concat(".", ext);
+async function downloadHighestQualityVideo(url, res) {
+  const videoFilter = {
+    filter: "videoonly",
+    quality: "highestvideo",
+    filter: (f) => f.container === "mp4",
+  };
+  const audioFilter = {
+    filter: "audioonly",
+    quality: "highestaudio",
+    filter: (f) => f.container === "mp4" && !f.hasVideo,
+  };
+
+  const info = await ytdl.getInfo(url);
+  let audioFormat = ytdl.chooseFormat(info.formats, audioFilter);
+  let videoFormat = ytdl.chooseFormat(info.formats, videoFilter);
+
+  res.attachment(`${info.videoDetails.title}.${videoFormat.container}`);
+  res.header("Content-Type", videoFormat.mimeType.split(";")[0]);
+
+  const audio = ytdl(url, { format: audioFormat });
+  const video = ytdl(url, { format: videoFormat });
+
+  const mergeStream = mergeAudioAndVideo(audio, video);
+
+  return mergeStream;
+}
+
+function mergeAudioAndVideo(audioStream, videoStream) {
+  const ffmpegProcess = cp.spawn(
+    ffmpeg,
+    [
+      // Remove ffmpeg's console spamming
+      ["-loglevel", "0", "-hide_banner"],
+
+      // Set inputs
+      ["-i", "pipe:3"],
+      ["-i", "pipe:4"],
+
+      // Map audio & video from streams
+      ["-map", "0:a"],
+      ["-map", "1:v"],
+
+      // Keep video encoding. encode audio as flac
+      ["-c:v", "copy"],
+      ["-c:a", "flac"],
+
+      ["-movflags", "isml+frag_keyframe"],
+
+      ["-f", "mp4", "pipe:5"],
+    ].flat(),
+    {
+      windowsHide: true,
+      stdio: [
+        /* Standard: stdin, stdout, stderr */
+        "inherit",
+        "inherit",
+        "inherit",
+        /* Custom: pipe:3, pipe:4, pipe:5 */
+        "pipe",
+        "pipe",
+        "pipe",
+      ],
+    }
+  );
+
+  audioStream.pipe(ffmpegProcess.stdio[3]);
+  videoStream.pipe(ffmpegProcess.stdio[4]);
+
+  return ffmpegProcess.stdio[5];
 }
