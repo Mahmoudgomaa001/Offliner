56df919aa0ac51ad6fdf11c8d139619ab49d44d2
Noureddine Laassari <nlaassari@gmail.com>
2024-04-19 15:03:52 +0100

    update dev workflow

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ devTarget: 'esnext' })],
   resolve: {
     alias: {
       '@': path.join(__dirname, 'src', '@'),
