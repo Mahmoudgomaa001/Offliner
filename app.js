20eddc69febce3bca41273486c9b6a2803764642
Noureddine Laassari <nlaassari@gmail.com>
2024-04-22 22:48:54 +0100

    fix production build

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ import express from 'express'
 import fs from 'fs'
 
 import VideoRoutes from './backend/routes/video.routes.js'
+import path from 'path'
 
 var app = express()
 const isProd = process.env.NODE_ENV === 'production'
+const __dirname = new URL('.', import.meta.url).pathname
+
 const indexPath = 'index.html'
-const indexProd = isProd ? fs.readFileSync(indexPath, 'utf-8') : ''
 const root = process.cwd()
 
-// app.use(express.static(path.join(__dirname, './public')))
 let vite
 
 if (!isProd) {
@@ -34,25 +35,24 @@ if (!isProd) {
 
 app.use('/api', VideoRoutes)
 
-app.use('*', async (req, res) => {
-  res.set({ 'Content-Type': 'text/html' })
+if (isProd) {
+  app.use(express.static(path.join(__dirname, 'dist')))
+} else {
+  app.use('*', async (req, res) => {
+    res.set({ 'Content-Type': 'text/html' })
 
-  try {
-    if (!isProd) {
+    try {
       const url = req.originalUrl
       let template = fs.readFileSync(indexPath, 'utf8')
       template = await vite.transformIndexHtml(url, template)
 
       res.status(200).end(template)
-      return
+    } catch (e) {
+      console.log(e.stack)
+      res.status(500).end(e)
     }
-
-    res.status(200).sendFile(indexProd)
-  } catch (e) {
-    console.log(e.stack)
-    res.status(500).end(e)
-  }
-})
+  })
+}
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server running!')
