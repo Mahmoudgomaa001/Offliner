d7edaeb67345786cc8674b002d5bc2d1344b1377
Noureddine Laassari <nlaassari@gmail.com>
2024-04-24 22:58:07 +0100

    update VideoCard styling

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -22,7 +22,10 @@ export default function VideoCard({ videoInfo, onDelete }: Props) {
             src={videoInfo.thumbnails?.at(-1)?.url}
             width={400}
           />
-          <p className="absolute bottom-2 right-2 bg-[#00000099] text-white rounded p-1 leading-none">
+          <p className="absolute bottom-2 left-2 bg-[#00000099] text-white rounded p-1 leading-none text-sm">
+            {humanFileSize(videoInfo.file.size)}
+          </p>
+          <p className="absolute bottom-2 right-2 bg-[#00000099] text-white rounded p-1 leading-none text-sm">
             {formatSeconds(+videoInfo.lengthSeconds)}
           </p>
         </div>
@@ -30,13 +33,13 @@ export default function VideoCard({ videoInfo, onDelete }: Props) {
       </Link>
       <h3 className="flex items-center">
         <Link
-          className="hover:underline text-base font-semibold leading-none flex-grow"
+          className="hover:underline text-base font-semibold leading-none flex-grow mr-1"
           to={videoLink}
         >
-          {videoInfo.title} ({humanFileSize(videoInfo.file.size)})
+          {videoInfo.title}
         </Link>
 
-        <p className="text-sm text-muted-foreground w-12 m-l-2">
+        <p className="text-sm text-muted-foreground w-12">
           {formatNumber(+videoInfo.viewCount)}
         </p>
         <Eye size={22} className="flex-shrink-0 text-muted-foreground" />
