cfb04e909e1e7752a03d651afa7a8445ff1ac669
Noureddine Laassari <nlaassari@gmail.com>
2024-07-26 16:31:45 +0100

    add auto play to global options

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -6,12 +6,13 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  DialogClose,
 } from '@/components/ui/dialog'
 import { Label } from '@/components/ui/label'
 import { Switch } from '@/components/ui/switch'
 import { Options, getAllOptions, setOption } from '@/lib/options'
 import { toast } from './ui/use-toast'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Button } from './ui/button'
 
 type Props = {
@@ -21,24 +22,36 @@ type Props = {
 export default function SettingsModal({ children }: Props) {
   const [loading, setLoading] = useState(true)
   const [useBgFetch, setUseBgFetch] = useState(false)
+  const [useAutoPlay, setUseAutoPlay] = useState(false)
+  const closeBtnRef = useRef<HTMLButtonElement>()
 
   useEffect(() => {
     getAllOptions()
       .then((options: Options) => {
         setUseBgFetch(options.useBgFetch)
+        setUseAutoPlay(options.autoPlay)
       })
       .finally(() => setLoading(false))
   }, [])
 
   async function saveChanges() {
-    await setOption('useBgFetch', useBgFetch)
-    toast({ title: 'Settings Updated!', duration: 900 })
+    Promise.all([
+      setOption('useBgFetch', useBgFetch),
+      setOption('autoPlay', useAutoPlay),
+    ])
+      .then(() => {
+        closeBtnRef.current.click()
+        toast({ title: 'Settings Updated!', duration: 900 })
+      })
+      .catch((err) => {
+        toast({ title: err.toString(), duration: 900, variant: 'destructive' })
+      })
   }
 
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[500px]">
         <DialogHeader>
           <DialogTitle>Settings</DialogTitle>
           <DialogDescription>change web app settings & flags</DialogDescription>
@@ -46,18 +59,38 @@ export default function SettingsModal({ children }: Props) {
         {loading ? (
           <p>Loading...</p>
         ) : (
-          <div className="grid gap-4 py-4">
-            <div className="flex items-center space-x-2">
+          <div className="space-y-6">
+            <div className="flex flex-wrap items-center space-x-2">
+              <Label htmlFor="bg-fetch" className="text-md flex-grow">
+                Background Fetch
+              </Label>
               <Switch
                 id="bg-fetch"
                 onCheckedChange={setUseBgFetch}
                 checked={useBgFetch}
               />
-              <Label htmlFor="bg-fetch">Background fetch</Label>
+              <p className="text-muted-foreground text-sm !ml-0 w-full">
+                Download videos in the background
+              </p>
+            </div>
+
+            <div className="flex flex-wrap items-center space-x-2">
+              <Label htmlFor="bg-fetch" className="text-md flex-grow">
+                Auto Play Videos
+              </Label>
+              <Switch
+                id="bg-fetch"
+                onCheckedChange={setUseAutoPlay}
+                checked={useAutoPlay}
+              />
+              <p className="text-muted-foreground text-sm !ml-0 w-full">
+                Auto play next video after current video has ended
+              </p>
             </div>
           </div>
         )}
         <DialogFooter>
+          <DialogClose ref={closeBtnRef}></DialogClose>
           <Button type="submit" onClick={saveChanges}>
             Save changes
           </Button>
