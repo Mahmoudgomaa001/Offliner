226f64c4cf92e0eb3714f87691bd049f17377ed4
Noureddine Laassari <nlaassari@gmail.com>
2024-08-12 16:26:17 +0100

    fix sentry error reporting

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import './instrument.js'
 
 import express from 'express'
 import fs from 'fs'
+import * as Sentry from '@sentry/node'
 
 import VideoRoutes from './backend/routes/video.routes.js'
 import path from 'path'
@@ -59,6 +60,8 @@ if (isProd) {
   })
 }
 
+Sentry.setupExpressErrorHandler(app)
+
 const port = process.env.PORT || 3000
 const server = app.listen(port, () => {
   logger.info(`Server running on port ${port}`)
@@ -66,6 +69,12 @@ const server = app.listen(port, () => {
 
 process.on('SIGTERM', handleServerShutdown)
 process.on('SIGINT', handleServerShutdown)
+process.on('uncaughtExceptionMonitor', (err) =>
+  handleUncaughtError(err, 'uncaughtExceptionMonitor')
+)
+process.on('unhandledRejection', (reason) =>
+  handleUncaughtError(reason, 'unhandledRejection')
+)
 
 function handleServerShutdown() {
   logger.info('Server is closing')
@@ -75,9 +84,16 @@ function handleServerShutdown() {
     process.exit(0)
   })
 
-  // Force close remaning connections
+  // Force close remaining connections
   setTimeout(() => {
     server.closeAllConnections()
     process.exit(1)
   }, 3000)
+}
+
+function handleUncaughtError(error, type) {
+  logger.child({ type }).error(error)
+  Sentry.captureException(error)
+
+  if (type !== 'uncaughtExceptionMonitor') process.exit(1)
 }
