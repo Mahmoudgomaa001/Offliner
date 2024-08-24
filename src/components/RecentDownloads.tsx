14047774d12f10085cd810b15004f35a1556e792
Noureddine Laassari <nlaassari@gmail.com>
2024-08-24 03:22:27 +0100

    fix recent downloads url for audio

--- src/components/RecentDownloads.tsx
+++ src/components/RecentDownloads.tsx
@@ -2,8 +2,12 @@ import { getAllVideos } from '@/lib/FileSystemManager'
 import useAsync from './hooks/useAsync'
 import { Loader, VideoOff } from 'lucide-react'
 import SmallVideoCard from './SmallVideoCard'
+import AudioCard from './AudioCard'
+import { Fragment } from 'react/jsx-runtime'
+import { useNavigate } from 'react-router-dom'
 
 export default function RecentDownloads() {
+  const navigate = useNavigate()
   const { loading, value: videos } = useAsync(() => getAllVideos({ count: 5 }))
 
   if (loading)
@@ -20,13 +24,23 @@ export default function RecentDownloads() {
   return (
     <div className="flex flex-col gap-5">
       {videos.map((v) => (
-        <SmallVideoCard
-          imgSrc={v.thumbnail}
-          title={v.title}
-          lengthSeconds={+v.lengthSeconds}
-          videoId={v.videoId}
-          key={v.videoId}
-        />
+        <Fragment key={v.videoId}>
+          {v.type === 'audio' ? (
+            <AudioCard
+              imgSrc={v.thumbnail}
+              title={v.title}
+              duration={v.lengthSeconds}
+              onClick={() => navigate(`/audio?id=${v.videoId}`)}
+            />
+          ) : (
+            <SmallVideoCard
+              imgSrc={v.thumbnail}
+              title={v.title}
+              lengthSeconds={v.lengthSeconds}
+              videoId={v.videoId}
+            />
+          )}
+        </Fragment>
       ))}
     </div>
   )
